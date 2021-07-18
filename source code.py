462cab87ebc34154a4c99b55ecfe798b947f74b9
Me-code04 <60747833+Me-code04@users.noreply.github.com>
2021-07-18 20:41:30 +0530

    Completed coding for subprogram 1.

--- source code.py
+++ source code.py
@@ -1,2 +1,8 @@
 #Mortgage Calculator
-#1
+#Subprogram1: Monthly Payment
+def Monthly_Payment (InterestRate, Principal, Num_MonthlyPayments):
+    Monthly_InterestRate = (InterestRate / 100) / 12
+    Monthly_Pay = (Monthly_InterestRate * Principal) / (1-(1+Monthly_InterestRate)**(-1 * Num_MonthlyPayments))
+    return Monthly_InterestRate
+    return Monthly_Pay
+#Subprogram2: Debt Schedule
