fb4e133ef2e0ba9c6f2410a4071562d1a7a0f81a
Me-code04 <60747833+Me-code04@users.noreply.github.com>
2021-07-19 13:19:11 +0530

    Completed Subprogram 3 and 4

--- source code.py
+++ source code.py
@@ -1,8 +1,19 @@
 #Mortgage Calculator
+Monthly_InterestRate = (InterestRate / 100) / 12
 #Subprogram1: Monthly Payment
-def Monthly_Payment (InterestRate, Principal, Num_MonthlyPayments):
-    Monthly_InterestRate = (InterestRate / 100) / 12
+def Monthly_Payment (Monthly_InterestRate, Principal, Num_MonthlyPayments):
     Monthly_Pay = (Monthly_InterestRate * Principal) / (1-(1+Monthly_InterestRate)**(-1 * Num_MonthlyPayments))
-    return Monthly_InterestRate
     return Monthly_Pay
 #Subprogram2: Debt Schedule
+def Debt_Schedule (Monthly_InterestRate, Principal, Num_MonthlyPayments):
+    ##for i in range(Num_MonthlyPayments):
+    ##Geometric Series Work
+    return None
+#Subprogram3: Total Interest
+def TotalInterest (Monthly_Pay, Num_MonthlyPayments, Principal):
+    Tot_i = (Monthly_Pay*Num_MonthlyPayments) - Principal
+    return Tot_i
+#Subprogram4: Cumulitive Interest
+def CumulitiveInterest (Monthly_Pay, n_payments, Monthly_InterestRate, Principal):
+    C_interest = (Principal * Monthly_InterestRate - Monthly_Pay) * ((((1 + Monthly_InterestRate) ** n_payments) - 1)/r) + (Monthly_Pay * n_payments)
+    return C_interest
